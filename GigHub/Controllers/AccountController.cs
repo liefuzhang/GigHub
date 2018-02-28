298089b4eabc048da6454749aa651e4c619f949d
liefu zhang <liefuzhangnz@gmail.com>
2018-02-28 21:09:39 +1300

    Enabled migrations

--- GigHub/Controllers/AccountController.cs
+++ GigHub/Controllers/AccountController.cs
@@ -30,7 +30,7 @@ namespace GigHub.Controllers
 
         public ApplicationSignInManager SignInManager
         {
-            get
+            get 
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
