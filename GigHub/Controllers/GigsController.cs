3d29aa603e4d817ea6fcc8077370516e5d02f86b
Liefu Zhang <liefu.zhang@aderant.com>
2018-03-01 15:45:35 +1300

    implement validation

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -25,9 +25,14 @@ namespace GigHub.Controllers {
         [HttpPost]
         [Authorize]
         public ActionResult Create(GigFormViewModel viewModel) {
+            if (!ModelState.IsValid) {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
             var gig = new Gig {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
