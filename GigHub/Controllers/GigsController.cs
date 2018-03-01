41e8a70a2f6553db57410a6c472aaabab2da7ee8
Liefu Zhang <liefu.zhang@aderant.com>
2018-03-01 13:48:15 +1300

    implement saving a gig;

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers {
     public class GigsController : Controller {
@@ -11,12 +13,29 @@ namespace GigHub.Controllers {
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create() {
             var viewModel = new GigFormViewModel {
                 Genres = _context.Genres.ToList()
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(GigFormViewModel viewModel) {
+            var gig = new Gig {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -5,8 +5,10 @@ namespace GigHub.Models {
     public class Gig {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -14,7 +16,9 @@ namespace GigHub.Models {
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
