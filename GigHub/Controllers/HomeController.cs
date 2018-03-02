50c7db486560d526f29d522166de35f412eb373b
Liefu Zhang <liefu.zhang@aderant.com>
2018-03-02 13:32:09 +1300

    improve home page UI

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -21,22 +21,10 @@ namespace GigHub.Controllers {
         public ActionResult Index() {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
-                .Where(g => g.DateTime > DateTime.Now)
-                .ToList();
+                .Include(g => g.Genre)
+                .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
-        }
-
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
